a942d16a5e4c6b1fa79f867baa7acefe4020655d
Adamya Chauhan <adamya1332004@gmail.com>
2026-07-07 17:37:33 +0530

    basic Setup

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,13 +5,7 @@ const tweetSchema = new mongoose.Schema({
         type: String,
         required: true,
         max: [250, 'Too long tweet']
-    },
-    hashtags : [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref : 'Hashtag'
-        }
-    ]
+    }
 }, {timestamps: true});
 
 const Tweet = mongoose.model('Tweet',tweetSchema);
