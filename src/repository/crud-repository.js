c8f56e88e8446cc7001f3a1408390d5906f12a1d
Adamya Chauhan <adamya1332004@gmail.com>
2026-07-10 17:12:30 +0530

    Basic crud repo done

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,56 @@
+class CrudRepository {
+    constructor(model) {
+        this.model = model;
+    }
+    async create(data) {
+        try {
+            const res = await this.model.create(data);
+            return res;
+        } catch (error) {
+            console.log('Something went wrong at the crud level');
+            throw error;
+        }
+    }
+
+    async destroy(id) {
+        try {
+            const res = await this.model.findByIdAndDelete(id);
+            return res;
+        } catch (error) {
+            console.log('Something went wrong at the crud level');
+            throw error;
+        }
+    }
+
+    async get(id) {
+        try {
+            const res = await this.model.findById(id);
+            return res;
+        } catch (error) {
+            console.log('Something went wrong at the crud level');
+            throw error;
+        }
+    }
+
+    async getAll() {
+        try {
+            const res = await this.model.find({});
+            return res;
+        } catch (error) {
+            console.log('Something went wrong at the crud level');
+            throw error;
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const res = this.model.findByIdAndUpdate(id, data, {new : true});
+            return res;
+        } catch (error) {
+            console.log('Something went wrong at the crud level');
+            throw error;
+        }
+    }
+}
+
+export default CrudRepository;
