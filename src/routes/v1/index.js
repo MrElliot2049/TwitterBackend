c28f5859a1484a75fd5ba8a4aa6d02fe3930ae40
Adamya Chauhan <adamya1332004@gmail.com>
2026-07-17 15:06:14 +0530

    auth fully done

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/tweets', authenticate, createTweet);
 router.post('/likes/toggle', toggleLike);
-router.post('/comments', createComment);
+router.post('/comments',authenticate, createComment);
 router.get('/tweet/:id', getTweet);
 router.post('/signup', signUp);
 router.post('/login', login);
